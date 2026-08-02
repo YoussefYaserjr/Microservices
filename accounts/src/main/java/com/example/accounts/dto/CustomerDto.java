dfc15987bed3ccce2ceef6550f99e3834500d44b
YoussefYaserjr <yy1170@fayoum.edu.eg>
2026-08-03 02:15:27 +0300

    initialization V1

---
+++ accounts/src/main/java/com/example/accounts/dto/CustomerDto.java
@@ -0,0 +1,25 @@
+package com.example.accounts.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+
+public class CustomerDto {
+
+
+    private String name;
+
+
+    private String email;
+
+
+    private String mobileNumber;
+
+
+    private AccountsDto accountsDto;
+}
